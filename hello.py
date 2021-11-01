9571b2d3d4c32851cac85e7ef16f119c8b421c14
markheinemann <mark_heinemann@yahoo.co.uk>
2021-11-01 16:12:10 +0000

    added hello.py

---
+++ hello.py
@@ -0,0 +1,30 @@
+print("hello world")
+print("first vscode comment")
+print("second vscode comment")
+
+
+print(" more stuff")
+
+
+print("  added to branch mycode")
+
+
+print( " 1st comment from win7 on line7")master
+
+
+print (" 1st comment from ubuntu")
+
+print( "comment added whilst on mycode branch") 
+print( "another comment added whilst on mycode branch")
+
+
+print(" the  really latest comment")mycode
+
+
+#
+
+this is the branch mycode
+ end of the file
+
+
+new line monday
